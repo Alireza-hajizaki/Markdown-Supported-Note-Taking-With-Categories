32473a786cdb8252516ccd120c2833b215fc4b49
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-30 14:08:43 +0330

    custom hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import {useMemo} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import NewNote from "./components/NewNote";
 import { Container } from "react-bootstrap";
+import { RawNote , Tag } from "./types/type";
+import {useLocalStorage} from "./Hooks/useLocalStorage";
 
 function App() {
+  const[notes,setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const[tags,setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note , tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  },[notes,tags])
+
   return (
     <Container className="my-4">
       <Routes>
