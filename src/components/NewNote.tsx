94b9f369a1de334f4b37adadb70b2de60ddea86a
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-07-31 23:56:47 +0330

    create note

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -1,11 +1,11 @@
 import NoteForm from "./NoteForm";
 import { NewNoteProps } from "../types/type";
 
-function NewNote( {onSubmit }: NewNoteProps) {
+function NewNote( {onSubmit , onAddTag , availableTags }: NewNoteProps) {
     return (
       <>
         <h1 className="mb-4 text-2xl font-bold text-gray-900 dark:text-white">New Note</h1>
-        <NoteForm onSubmit={onSubmit}/>
+        <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags}/>
       </>
     )
   }
